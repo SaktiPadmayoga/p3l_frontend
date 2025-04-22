725948e46426f1edab33bd7a1a8e209050dc3901
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-04-22 19:15:03 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './layouts/Dashboard';
-import ManagePegawai from './components/ManagePegawai';
+import Dashboard from './layouts/Administrator/Dashboard';
+import Login from './auth/login/login';
+import Register from './auth/register/register';
+import HomePage from './layouts/user/pages/homepage';
+import UserNavbar from './layouts/user/components/use-navbar';
+import DetailProduct from './layouts/user/pages/detail-product';
 
 function App() {
+  const isLoggedIn = false;
+
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<Dashboard />} />
-        {/* <Route path="/manage-pegawai" element={<ManagePegawai />} /> */}
-            {/* <Route path="/users" element={<Users />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/email" element={<Email />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/help" element={<Help />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Dashboard />} />
+        <Route path="/detail-product" element={
+          <>
+            <UserNavbar isLoggedIn={isLoggedIn} />
+            <DetailProduct />
+          </>
+        } />
+        <Route path="*" element={
+          <>
+            <UserNavbar isLoggedIn={isLoggedIn} />
+            <HomePage />
+          </>
+        } />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-

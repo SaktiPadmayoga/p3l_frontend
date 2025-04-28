fb3c4e7624f222c33e20b93d43483d371cef0847
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-04-28 16:18:08 +0700

    Update route

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom
 import Dashboard from './layouts/Administrator/Dashboard';
 import Login from './auth/login/login';
 import Register from './auth/register/register';
-import HomePage from './layouts/user/pages/homepage';
-import Catalogue from './layouts/user/pages/catalogue';
-import UserNavbar from './layouts/user/components/user-navbar';
-import Footer from './layouts/user/components/footer';
+import HomePage from './layouts/user/pages/Homepage';
+import Catalogue from './layouts/user/pages/Catalogue';
+import UserNavbar from './layouts/user/components/UserNavbar';
 import DetailProduct from './layouts/user/pages/detail-product';
 import Profile from './layouts/user/pages/profile';
 import ManagePegawai from './layouts/administrator/pages/manage-pegawai';
@@ -57,7 +56,7 @@ function App() {
         {/* Routes WITH navbar and footer */}
         <Route element={<UserLayout isLoggedIn={isLoggedIn} />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/Catalogue" element={<Catalogue />} />
           <Route path="/detail-product" element={<DetailProduct />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<HomePage />} />
